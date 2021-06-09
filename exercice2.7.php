1a54fa55b899ca880343f7c6dc9a617017e88125
Ines Bakhtaoui <inesbkht@gmail.com>
2021-06-09 15:18:25 +0200

    correction ajout HTML

--- exercice2.7.php
+++ exercice2.7.php
@@ -1,11 +1,25 @@
-<?php
-$isOk = false;
-if ($isOk == false)
-{
-    echo "C'est pas bon !!!";
-}
-else
-{
-    echo "C'est ok !!";
-}
-?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 2.7</title>
+</head>
+<body>
+    <h1>Exercice 7 Partie 2 : Les conditions PHP</h1>
+    <p>
+       <?php
+        $isOk = false;
+        if ($isOk == false)
+        {
+            echo "C'est pas bon !!!";
+        }
+        else
+        {
+            echo "C'est ok !!";
+        }
+        ?>
+    </p>
+</body>
+</html>
